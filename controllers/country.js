12e80846cc390e068db2e369e95cd5ebdbdbfc4f
farukh110 <farukhsajjad110@gmail.com>
2024-08-10 16:26:20 +0500

    i created country controller simple

---
+++ controllers/country.js
@@ -0,0 +1,13 @@
+const Country = require('../models/country');
+const asyncHandler = require('express-async-handler');
+
+// create country
+
+// update country
+
+// delete country
+
+// get all countries
+
+// get country
+
