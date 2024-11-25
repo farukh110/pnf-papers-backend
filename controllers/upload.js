60e08678d16ac8603790ef0c4317a8f7c1b1d646
farukh110 <farukhsajjad110@gmail.com>
2024-11-25 23:40:37 +0500

    created upload controller

---
+++ controllers/upload.js
@@ -0,0 +1,73 @@
+// upload images
+
+const uploadImages = asyncHandler(async (req, res) => {
+
+    // console.log('uploaded files: ', req.files);
+
+    try {
+
+        // const { id } = req.params;
+
+        const uploader = (path) => cloudinaryUploadImage(path, "images");
+
+        const imagesUrls = [];
+
+        const imageFiles = req.files;
+
+        for (const item of imageFiles) {
+
+            const { path } = item;
+
+            const newPath = await uploader(path);
+
+            imagesUrls.push(newPath);
+
+            fs.unlinkSync(path);
+        }
+
+        const images = imagesUrls.map((fileItem) => {
+
+            return fileItem;
+
+        });
+
+        res.json(images);
+
+        // const product = await Product.findByIdAndUpdate(id, {
+
+        //     images: imagesUrls.map((fileItem) => {
+
+        //         return fileItem;
+
+        //     })
+
+        // }, {
+        //     new: true
+        // });
+
+
+    } catch (error) {
+
+        throw new Error(error);
+    }
+
+});
+
+// delete images
+
+const deleteImages = asyncHandler(async (req, res) => {
+
+    try {
+
+        const { id } = req.params;
+
+        const removeImages = cloudinaryDeleteImage(id, "images");
+
+        res.json({ message: "Image has been Deleted" });
+
+    } catch (error) {
+
+        throw new Error(error);
+    }
+
+});
